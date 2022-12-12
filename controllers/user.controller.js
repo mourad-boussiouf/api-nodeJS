a8024fcff6106c79da3b435ed511fbdb6ec28f91
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-12-12 23:58:31 +0100

    ajout indications sur utilisation des routes readme doc api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -162,7 +162,7 @@ exports.adminManageUserById = (req, res) => {
       res.status(500).send({ message: err.message });
     });
   }
-//adminmodify
+//adminmodify2
   exports.adminModifyGroupeBelongingByUsername = (req, res) => {
     User.update({
       groupeId: req.body.groupeId
